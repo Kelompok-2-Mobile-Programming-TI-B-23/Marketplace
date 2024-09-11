689e694daef29b4927412eb7b041d26f587ae609
Stefi1011 <153803440+Stefi1011@users.noreply.github.com>
2024-09-11 23:42:18 +0700

    [UPDATE] Membuat branch baru
    - Menambahkan font yang dibutuhkan
    - Membuat file untuk screen ewallet

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:marketplace/e_wallet.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +11,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Urbanist'),
+      home: const EWalletScreen()
+    );
   }
 }
