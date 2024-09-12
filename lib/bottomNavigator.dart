0daf512aff1bf7ab0dab66c6273691c8f09a8654
jclynviaa <jocelyn.535230046@stu.untar.ac.id>
2024-09-12 21:56:44 +0700

    Ubah Design & Penggunaan Bottom Navigation Bar
    1. mengubah Design & Penggunaan Bottom Navigation Bar
    2. bikin navbar jadi ada animasi nya (pake package)
    3. nambahin text untuk setiap icon pada navbar

--- lib/bottomNavigator.dart
+++ lib/bottomNavigator.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class BottomNavigator extends StatefulWidget {
   const BottomNavigator({super.key});
@@ -14,57 +15,36 @@ class _BottomNavigatorState extends State<BottomNavigator> {
         child: Scaffold(
       backgroundColor: const Color(0xFFFFF8F0),
       body: SizedBox(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          child: Column(children: [
-            const SizedBox(height: 50),
-          ])),
-      bottomNavigationBar: BottomAppBar(
-        elevation: 1,
-        height: 80,
-        color: Colors.white,
-        shape: const CircularNotchedRectangle(),
-        notchMargin: 10,
-        clipBehavior: Clip.antiAliasWithSaveLayer,
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.home_outlined,
-                size: 30,
-                color: Colors.black,
-              ),
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.storefront_outlined,
-                size: 30,
-                color: Colors.black,
-              ),
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.shopping_cart_outlined,
-                size: 30,
-                color: Colors.black,
-              ),
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.account_circle_outlined,
-                size: 30,
-                color: Colors.black,
-              ),
-            ),
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          children: const [
+            SizedBox(height: 50),
           ],
         ),
       ),
+      bottomNavigationBar: Container(
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20),
+            child: GNav(
+              backgroundColor: Colors.white,
+              color: Colors.black,
+              activeColor: const Color(0xFF92140C),
+              tabBackgroundColor: const Color(0xFFEDEAE8),
+              gap: 8,
+              onTabChange: (index) {
+                print(index);
+              },
+              padding: EdgeInsets.all(10),
+              tabs: const [
+                GButton(icon: Icons.home_outlined, text: 'Home'),
+                GButton(icon: Icons.store_outlined, text: 'Store'),
+                GButton(icon: Icons.shopping_cart_outlined, text: 'Cart'),
+                GButton(icon: Icons.account_circle_outlined, text: 'Profile'),
+              ],
+            ),
+          )),
     ));
   }
 }
