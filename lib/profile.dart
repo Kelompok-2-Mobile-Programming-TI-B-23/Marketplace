cb0e2fc329494aadc92c946eb3c81cb152ae56f9
James Anderson <jamesandrn@gmail.com>
2024-09-14 14:33:08 +0700

    Update profile.dart

--- lib/profile.dart
+++ lib/profile.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:marketplace/login.dart';
 
+// commit profile
 class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
