76817feaeb2457bf6407305e82b7502b79e9ec15
eilleendwe <eilleenyeoh3@gmail.com>
2024-09-15 23:30:08 +0700

    Memperbaiki tampilan homepage menjadi lebih bagus
    - sudah bisa melihat barang per category maupun semua category (tapi data masih hardcode)
    - categories bisa di scroll secara horizontal
    - menambahkan icon untuk categories (menggunakan package icon plus)
    - semua masih dilakukan di homepage.dart, next step akan memindahkan menjadi per file dan merapikan file homepage.dart

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,7 +1,5 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_typeahead/flutter_typeahead.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'bottomNavigator.dart'; // Import BottomNavigator
 import 'package:icons_plus/icons_plus.dart';
@@ -76,6 +74,62 @@ class _HomeScreenState extends State<HomeScreen> {
 
   int _activePage = 0; // Gambar pertama
 
+  String selectedCategory = 'All'; // Default cateogry
+
+  // Dummy data
+  final List<Map<String, dynamic>> allProducts = [
+    {
+      'name': 'T-Shirt 1',
+      'category': 'T-Shirt',
+      'price': 'Rp 100.000',
+      'rating': 4.5
+    },
+    {
+      'name': 'T-Shirt 2',
+      'category': 'T-Shirt',
+      'price': 'Rp 150.000',
+      'rating': 4.0
+    },
+    {
+      'name': 'Pants 1',
+      'category': 'Pants',
+      'price': 'Rp 200.000',
+      'rating': 3.5
+    },
+    {
+      'name': 'Shoes 1',
+      'category': 'Shoes',
+      'price': 'Rp 300.000',
+      'rating': 5.0
+    },
+    {
+      'name': 'Accessoryyyyyyyyyyy 1',
+      'category': 'Accessory',
+      'price': 'Rp 50.000',
+      'rating': 3.0
+    },
+  ];
+
+  // Categories
+  final List<String> categories = [
+    'All',
+    'T-Shirt',
+    'Pants',
+    'Shoes',
+    'Accessory'
+  ];
+
+  // Ambil product berdasarkan category
+  List<Map<String, dynamic>> get filteredProducts {
+    if (selectedCategory == 'All') {
+      return allProducts;
+    } else {
+      return allProducts
+          .where((product) => product['category'] == selectedCategory)
+          .toList();
+    }
+  }
+
   void startTimer() {
     _timer = Timer.periodic(Duration(seconds: 3), (timer) {
       if (_pageController.page == imagePaths.length - 1) {
@@ -130,6 +184,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 children: [
                   Row(
                     children: [
+                      // Search bar
                       Expanded(
                         child: TextField(
                           decoration: InputDecoration(
@@ -172,6 +227,8 @@ class _HomeScreenState extends State<HomeScreen> {
                   SizedBox(
                     height: 20,
                   ),
+
+                  // Page View untuk gambar diskon / produk yang lagi hots
                   SizedBox(
                     width: double.infinity,
                     height: MediaQuery.of(context).size.height / 4,
@@ -192,6 +249,8 @@ class _HomeScreenState extends State<HomeScreen> {
                   SizedBox(
                     height: 10,
                   ),
+
+                  // Indikator halaman slide page view
                   Container(
                     color: Colors.transparent,
                     child: Row(
@@ -220,6 +279,8 @@ class _HomeScreenState extends State<HomeScreen> {
                   SizedBox(
                     height: 10,
                   ),
+
+                  // Category Text
                   Align(
                     alignment: Alignment.centerLeft,
                     child: Text(
@@ -232,13 +293,162 @@ class _HomeScreenState extends State<HomeScreen> {
                       ),
                     ),
                   ),
+                  SizedBox(height: 20),
+
+                  // Horizontal Category Buttons
                   SingleChildScrollView(
                     scrollDirection: Axis.horizontal,
                     child: Row(
-                      children: [],
+                      children: categories.map((category) {
+                        return GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              selectedCategory =
+                                  category; // Mengubah category yang lagi dipilih
+                            });
+                          },
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 10.0),
+                            child: Column(
+                              children: [
+                                // Container bulat untuk icon
+                                Container(
+                                  padding: EdgeInsets.all(15),
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: selectedCategory == category
+                                        ? Color.fromARGB(255, 146, 20, 12)
+                                        : Colors.grey[
+                                            300], // Warna berubah saat category dipilih
+                                  ),
+                                  child: Icon(
+                                    category == 'T-Shirt'
+                                        ? MingCute.t_shirt_line
+                                        : category == 'Pants'
+                                            ? MingCute.shorts_line
+                                            : category == 'Shoes'
+                                                ? MingCute.shoe_line
+                                                : category == 'Accessory'
+                                                    ? MingCute.watch_2_line
+                                                    : Icons
+                                                        .grid_view, // Ikon default
+                                    color: Colors.white,
+                                    size: 24,
+                                  ),
+                                ),
+                                SizedBox(height: 5),
+                                Text(
+                                  category,
+                                  style: TextStyle(
+                                    color: selectedCategory == category
+                                        ? Color.fromARGB(255, 30, 30, 36)
+                                        : Colors.grey,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      }).toList(),
                     ),
                   ),
-                  SizedBox(height: 16),
+
+                  SizedBox(height: 20),
+
+                  // Product Grid
+                  GridView.builder(
+                      physics: NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 10,
+                        mainAxisSpacing: 10,
+                        childAspectRatio: 3 / 4,
+                      ),
+                      itemCount: filteredProducts
+                          .length, // Jumlah produk yang ditampilkan
+                      itemBuilder: (context, index) {
+                        // Mengambil data produk
+                        final product = filteredProducts[index];
+                        return Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          elevation: 3,
+                          child: Column(
+                            children: [
+                              // Placeholder untuk gambar produk masih dummy
+                              Container(
+                                height: 130,
+                                color: Colors.grey[200],
+                                child: Center(
+                                  child: Icon(
+                                    Icons.image,
+                                    size: 50,
+                                    color: Colors.grey[400],
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Column(
+                                  crossAxisAlignment:
+                                      CrossAxisAlignment.stretch,
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        // Nama Product
+                                        Expanded(
+                                          child: Text(
+                                            product['name'],
+                                            style: TextStyle(fontSize: 12),
+                                            maxLines:
+                                                1, // Melimit title produk agar 1 baris
+                                            overflow: TextOverflow
+                                                .ellipsis, // Menambahkan ... jika title produk terlalu panjang
+                                          ),
+                                        ),
+
+                                        SizedBox(
+                                          width: 5,
+                                        ),
+                                        // Rating
+                                        Row(
+                                          children: [
+                                            Icon(Icons.star,
+                                                color: Colors.yellow, size: 16),
+                                            SizedBox(width: 4),
+                                            Text(
+                                              product['rating'].toString(),
+                                              style: TextStyle(fontSize: 10),
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                    SizedBox(height: 5),
+
+                                    // Harga Produk
+                                    Text(
+                                      product['price'],
+                                      style: TextStyle(
+                                          color:
+                                              Color.fromARGB(255, 30, 30, 36),
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      }),
+                  SizedBox(height: 8.0),
                 ],
               ),
             ),
@@ -249,6 +459,7 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 }
 
+// Placeholder untuk gambar dalam slider (carousel)
 class ImagePlaceholder extends StatelessWidget {
   final String? imagePath;
   const ImagePlaceholder({super.key, this.imagePath});
@@ -259,6 +470,7 @@ class ImagePlaceholder extends StatelessWidget {
   }
 }
 
+// Widget untuk layar Toko
 class StoreScreen extends StatelessWidget {
   const StoreScreen({super.key});
 
@@ -270,6 +482,7 @@ class StoreScreen extends StatelessWidget {
   }
 }
 
+// Widget untuk layar Keranjang
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
 
@@ -281,6 +494,7 @@ class CartScreen extends StatelessWidget {
   }
 }
 
+// Widget untuk layar Profil
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
