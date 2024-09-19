c6bed03cfe20da311eb598bb34c8183e8ed0833f
Raymond Euginio <rayeuginio@gmail.com>
2024-09-19 21:31:11 +0700

    Menambahkan bottomappbar untuk informasi harga dan add to cart

--- lib/product_detail.dart
+++ lib/product_detail.dart
@@ -7,148 +7,202 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 255, 248, 240),
-      appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 255, 248, 240),
-        elevation: 0,
-        leading: IconButton(
-          icon: const Icon(CupertinoIcons.back, color: Colors.black),
-          onPressed: () {
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(
-                  builder: (context) =>
-                      LoginScreen()), // sementara ke login dlu
-            );
-          },
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 255, 248, 240),
+          elevation: 0,
+          leading: IconButton(
+            icon: const Icon(CupertinoIcons.back, color: Colors.black),
+            onPressed: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                    builder: (context) =>
+                        LoginScreen()), // sementara ke login dlu
+              );
+            },
+          ),
+          centerTitle: true,
+          title: Image.asset(
+            'assets/images/clothify.png',
+            height: 40,
+          ),
         ),
-        centerTitle: true,
-        title: Image.asset(
-          'assets/images/clothify.png',
-          height: 40,
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30.0),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Center(
-                child: Text(
-                  'Product Detail',
-                  style: GoogleFonts.urbanist(
-                    textStyle: const TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      color: Color.fromARGB(255, 146, 20, 12),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30.0),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 10),
+                Container(
+                  height: 300,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/gambar.png'),
+                      fit: BoxFit.cover,
                     ),
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.grey[200],
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.2),
+                        spreadRadius: 2,
+                        blurRadius: 8,
+                        offset: Offset(0, 4),
+                      ),
+                    ],
                   ),
                 ),
-              ),
-              const SizedBox(height: 10),
-              Container(
-                height: 300,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/images/gambar.png'),
-                    fit: BoxFit.cover,
-                  ),
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.grey[200],
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.2),
-                      spreadRadius: 2,
-                      blurRadius: 8,
-                      offset: Offset(0, 4),
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Product Name',
+                      style: GoogleFonts.urbanist(
+                        textStyle: const TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 146, 20, 12),
+                        ),
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.star,
+                          color: Colors.amber,
+                          size: 24,
+                        ),
+                        const SizedBox(width: 4),
+                        Text(
+                          '5.0',
+                          style: GoogleFonts.urbanist(
+                            textStyle: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black54,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
-              ),
-              const SizedBox(height: 16),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Product Name',
-                    style: GoogleFonts.urbanist(
-                      textStyle: const TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        color: Color.fromARGB(255, 146, 20, 12),
-                      ),
+                const SizedBox(height: 2),
+                Text(
+                  'Category Name',
+                  style: GoogleFonts.urbanist(
+                    textStyle: const TextStyle(
+                      fontSize: 14,
+                      color: Colors.grey,
                     ),
                   ),
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.star,
-                        color: Colors.amber,
-                        size: 24,
+                ),
+                const SizedBox(height: 2),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.store,
+                      color: Color.fromARGB(255, 146, 20, 12),
+                      size: 24,
+                    ),
+                    const SizedBox(width: 4),
+                    Text(
+                      'Store Name',
+                      style: GoogleFonts.urbanist(
+                        textStyle: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black54,
+                        ),
                       ),
-                      const SizedBox(width: 4),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 18),
+                Text(
+                  '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."',
+                  style: GoogleFonts.urbanist(
+                    textStyle: const TextStyle(
+                      fontSize: 16,
+                      color: Colors.black54,
+                    ),
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+              ],
+            ),
+          ),
+        ),
+        bottomNavigationBar: ClipRRect(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20.0),
+            topRight: Radius.circular(20.0),
+          ),
+          child: BottomAppBar(
+            // color: Color.fromARGB(255, 255, 248, 240),
+            color: Colors.white,
+            elevation: 100.0,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
                       Text(
-                        '5.0',
+                        'Price',
                         style: GoogleFonts.urbanist(
-                          textStyle: const TextStyle(
-                            fontSize: 18,
+                          textStyle: TextStyle(
+                            fontSize: 14,
                             fontWeight: FontWeight.bold,
-                            color: Colors.black54,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        'Rp. 0.00',
+                        style: GoogleFonts.urbanist(
+                          textStyle: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
                           ),
                         ),
                       ),
                     ],
                   ),
-                ],
-              ),
-              const SizedBox(height: 2),
-              Text(
-                'Category Name',
-                style: GoogleFonts.urbanist(
-                  textStyle: const TextStyle(
-                    fontSize: 14,
-                    color: Colors.grey,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 2),
-              Row(
-                children: [
-                  Icon(
-                    Icons.store,
-                    color: Color.fromARGB(255, 146, 20, 12),
-                    size: 24,
-                  ),
-                  const SizedBox(width: 4),
-                  Text(
-                    'Store Name',
-                    style: GoogleFonts.urbanist(
-                      textStyle: const TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black54,
+                  Spacer(),
+                  ElevatedButton(
+                    onPressed: () {
+                      // add logic nanti
+                    },
+                    style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.symmetric(horizontal: 40),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        backgroundColor: Color.fromARGB(255, 146, 20, 12)),
+                    child: Text(
+                      'Add to Cart',
+                      style: GoogleFonts.urbanist(
+                        textStyle: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
-                  ),
+                  )
                 ],
               ),
-              const SizedBox(height: 18),
-              Text(
-                '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."',
-                style: GoogleFonts.urbanist(
-                  textStyle: const TextStyle(
-                    fontSize: 16,
-                    color: Colors.black54,
-                  ),
-                ),
-                textAlign: TextAlign.left,
-              ),
-            ],
+            ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
