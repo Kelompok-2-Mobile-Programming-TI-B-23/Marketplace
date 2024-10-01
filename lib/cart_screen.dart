2250dbc2f9a8cca9e97e93c8ccfa949532613b6d
Stefi1011 <153803440+Stefi1011@users.noreply.github.com>
2024-10-02 01:30:28 +0700

    [FIX] screen di cartnya udah bener setengah (tinggal tombol)

--- lib/cart_screen.dart
+++ lib/cart_screen.dart
@@ -1,10 +1,11 @@
-// cart_screen.dart
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:intl/intl.dart'; // Import intl package for currency formatting
 import 'cart_service.dart';
 import 'cart_item_model.dart';
 import 'cart_empty_screen.dart'; // Import the EmptyCartScreen
+import 'checkout_screen.dart';
 
 class CartScreen extends StatefulWidget {
   const CartScreen({super.key});
@@ -16,7 +17,10 @@ class CartScreen extends StatefulWidget {
 class _CartScreenState extends State<CartScreen> {
   final CartService _cartService = CartService();
   User? user;
-  late Future<List<Map<String, dynamic>>> cartItemsFuture = Future.value([]); // Change this line
+  late Future<List<Map<String, dynamic>>> cartItemsFuture = Future.value([]);
+
+  final NumberFormat currencyFormat =
+      NumberFormat.currency(locale: 'id_ID', symbol: 'Rp ', decimalDigits: 0);
 
   @override
   void initState() {
@@ -29,7 +33,6 @@ class _CartScreenState extends State<CartScreen> {
       setState(() {
         user = currentUser;
         if (user != null) {
-          // Update the cartItemsFuture when the user is authenticated
           cartItemsFuture = _cartService.getCartItemsWithDetails(user!.uid);
         }
       });
@@ -39,8 +42,15 @@ class _CartScreenState extends State<CartScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 255, 248, 240),
       appBar: AppBar(
-        title: const Text('Cart'),
+        title: const Text('My Cart',
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: 18,
+                fontWeight: FontWeight.bold)),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 255, 248, 240),
       ),
       body: FutureBuilder<List<Map<String, dynamic>>>(
         future: cartItemsFuture,
@@ -50,11 +60,12 @@ class _CartScreenState extends State<CartScreen> {
           }
 
           if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            // Navigate to the custom empty cart screen if the cart is empty
+            // Navigate to the empty cart screen if the cart is empty
             Future.delayed(Duration.zero, () {
               Navigator.pushReplacement(
                 context,
-                MaterialPageRoute(builder: (context) => const CartEmptyScreen()),
+                MaterialPageRoute(
+                    builder: (context) => const CartEmptyScreen()),
               );
             });
             return const SizedBox(); // Return an empty widget
@@ -66,14 +77,107 @@ class _CartScreenState extends State<CartScreen> {
             itemCount: cartItems.length,
             itemBuilder: (context, index) {
               final item = cartItems[index];
+              final int totalPrice = item['cartItem'].quantity * item['price'];
+
               return ListTile(
-                title: Text(item['name']), // Access name from the map
-                subtitle: Text('${item['cartItem'].quantity} x ${item['price']}'), // Access quantity and price
-                trailing: Text('Total: ${item['cartItem'].quantity * item['price']}'), // Calculate total
-                onLongPress: () =>
-                    _cartService.removeItemFromCart(user!.uid, item['cartItem'].productId), // Access productId from the cart item
+                title: Text(item['name']),
+                subtitle: Text(
+                    '${item['cartItem'].quantity} x ${currencyFormat.format(item['price'])}'),
+                trailing: Text(
+                    'Total: ${currencyFormat.format(totalPrice)}'),
+                onLongPress: () => _cartService.removeItemFromCart(
+                    user!.uid, item['cartItem'].productId),
               );
             },
+          );
+        },
+      ),
+      bottomSheet: FutureBuilder<List<Map<String, dynamic>>>(
+        future: cartItemsFuture,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const SizedBox(); // Return empty if no items
+          }
+
+          final cartItems = snapshot.data!;
+          final totalAmount = cartItems.fold<double>(
+              0,
+              (sum, item) =>
+                  sum + (item['cartItem'].quantity * (item['price'] as num)).toDouble());
+
+          return Container(
+            height: 150,
+            padding: const EdgeInsets.all(16.0),
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 255, 248, 240),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: const Offset(0, 3),
+                ),
+              ],
+              borderRadius:
+                  const BorderRadius.vertical(top: Radius.circular(10)),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.all(8),
+                      child: Text(
+                        'Grand Total',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Color.fromARGB(255, 152, 150, 150),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8),
+                      child: Text(
+                        currencyFormat.format(totalAmount),
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 60, vertical: 10),
+                    backgroundColor:
+                        const Color.fromARGB(255, 146, 20, 12),
+                  ),
+                  onPressed: () {
+                    // Navigate to checkout screen
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const CheckoutScreen()),
+                    );
+                  },
+                  child: const Text(
+                    'Checkout',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           );
         },
       ),
