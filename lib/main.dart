13b29ac5bbc1c660be0673f6faac302e8fa9927e
Raymond Euginio <rayeuginio@gmail.com>
2024-10-01 17:06:34 +0700

    push main sama pubspec ketinggalan

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,37 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'product_detail.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:marketplace/auth/login.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:marketplace/product_detail.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
+  WidgetsFlutterBinding
+      .ensureInitialized(); // Ensure Flutter binding is initialized
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ProductDetailScreen(productId: 'product1'),
+      theme: ThemeData(
+        // Font Urbanist ke seluruh app
+        textTheme: GoogleFonts.urbanistTextTheme(
+          Theme.of(context).textTheme,
+        ),
+        textSelectionTheme: const TextSelectionThemeData(
+          cursorColor: Colors.black, // Warna kursor
+          selectionHandleColor: Color(0xFF92104C),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const SafeArea(child: ProductDetailScreen(productId: 'product1')),
     );
   }
 }
