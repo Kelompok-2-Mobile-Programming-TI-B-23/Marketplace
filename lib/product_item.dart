676783c170e7ea613136530d15e291edbff000c0
angel0060 <angelica.535230060@stu.untar.ac.id>
2024-10-05 10:38:56 +0700

    [UPDATE] membuat tiap product ada redirect ke product detail screen

--- lib/product_item.dart
+++ lib/product_item.dart
@@ -1,6 +1,9 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'product_detail.dart';
 
 class ProductItem extends StatelessWidget {
   final String productId;
@@ -21,72 +24,84 @@ class ProductItem extends StatelessWidget {
           .get(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(child: CircularProgressIndicator());
+          return Center(
+              child: CircularProgressIndicator(
+            color: Color.fromARGB(255, 146, 20, 12),
+          ));
         } else if (snapshot.hasError) {
           return Center(child: Text('Error: ${snapshot.error}'));
         } else if (!snapshot.hasData || !snapshot.data!.exists) {
           return Center(child: Text('Product not found'));
         }
 
-        // Extract product data
         var productData = snapshot.data!.data() as Map<String, dynamic>;
         String productName = productData['name'] ?? 'Unknown Product';
         String category = productData['category'] ?? 'Unknown Category';
-        int price = (productData['price'] ?? 100) as int;
+        int price = (productData['price'] ?? 0) as int;
         String productImage = productData['image'];
 
-        return Row(
-          children: [
-            Container(
-              width: 68,
-              height: 68,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                image: DecorationImage(
-                  image: NetworkImage(productImage),
-                  fit: BoxFit.cover,
+        return GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ProductDetailScreen(productId: productId),
+              ),
+            );
+          },
+          child: Row(
+            children: [
+              Container(
+                width: 68,
+                height: 68,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  image: DecorationImage(
+                    image: NetworkImage(productImage),
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
-            ),
-            SizedBox(width: 16),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      Text(
-                        productName,
-                        style: GoogleFonts.urbanist(
-                          textStyle: TextStyle(fontWeight: FontWeight.bold),
+              SizedBox(width: 16),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          productName,
+                          style: GoogleFonts.urbanist(
+                            textStyle: TextStyle(fontWeight: FontWeight.bold),
+                          ),
                         ),
-                      ),
-                      Spacer(),
-                      Text(
-                        'x$quantity',
-                        style: GoogleFonts.urbanist(
-                          textStyle: TextStyle(color: Colors.grey.shade700),
+                        Spacer(),
+                        Text(
+                          'x$quantity',
+                          style: GoogleFonts.urbanist(
+                            textStyle: TextStyle(color: Colors.grey.shade700),
+                          ),
                         ),
+                      ],
+                    ),
+                    Text(
+                      category,
+                      style: GoogleFonts.urbanist(
+                        textStyle: TextStyle(color: Colors.grey.shade700),
                       ),
-                    ],
-                  ),
-                  Text(
-                    category,
-                    style: GoogleFonts.urbanist(
-                      textStyle: TextStyle(color: Colors.grey.shade700),
                     ),
-                  ),
-                  SizedBox(height: 8),
-                  Text(
-                    '\Rp$price',
-                    style: GoogleFonts.urbanist(
-                      textStyle: TextStyle(fontWeight: FontWeight.w900),
+                    SizedBox(height: 8),
+                    Text(
+                      'Rp$price',
+                      style: GoogleFonts.urbanist(
+                        textStyle: TextStyle(fontWeight: FontWeight.w900),
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         );
       },
     );
